a307e75d2ba6799b13b809801068c030ce3b831a
Veronica Trias <vero.ramm@gmail.com>
2022-05-04 14:20:56 -0300

    Se modifico el Js por completo

--- js/main.js
+++ js/main.js
@@ -1,57 +1,17 @@
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	
-}
-
-const campos = {
-	nombre: false,
-    apellido: false,
-	correo: false
-}
-
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
-		break;
-        case "apellido":
-			validarCampo(expresiones.apellido, e.target, 'apellido');
-		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
-		break;
-		
-	}
-}
-const validarCampo= (expresion, input, campo) => {
-	if (expresion.test(input.value)) {
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelectorAll(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__grupo__input-error-activo');
-		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorresto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelectorAll(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__grupo__input-error-activo');
-		
-	}
-}
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
+const formulario = document.querySelector('#formulario');
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const email = document.querySelector('#email');
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
-
-    if (campos.nombre && campos.apellido && campos.correo){
-		alert('Formulario enviado exitosamente');
-        formulario.reset();
-		
-    } else {
-        alert('Por favor, llene todos los campos correctamente.');
-    }
+	if (nombre.value === '' || apellido.value === '' || email.value === '') {
+		alert('Todos los campos son obligatorios');
+		formulario.reset();
+	} else {
+		alert('Formulario enviado');
+		formulario.reset();
+	}	
 });
+
 
