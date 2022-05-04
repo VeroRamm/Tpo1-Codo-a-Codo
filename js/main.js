918bb24adb9304a97a42b1211bbfaa12f4828743
Veronica Trias <vero.ramm@gmail.com>
2022-05-04 14:58:06 -0300

    modifique estilos

--- js/main.js
+++ js/main.js
@@ -3,15 +3,30 @@ const nombre = document.querySelector('#nombre');
 const apellido = document.querySelector('#apellido');
 const email = document.querySelector('#email');
 
+
+
+
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
-	if (nombre.value === '' || apellido.value === '' || email.value === '') {
-		alert('Todos los campos son obligatorios');
-		formulario.reset();
-	} else {
-		alert('Formulario enviado');
-		formulario.reset();
-	}	
-});
+	
+	if(nombre.value.length == 0){
+		alert("Debe ingresar un nombre");
+		  user.focus();
+		  return false;
+	  }else if(apellido.value.length == 0){
+		alert("Debe ingresar un apellido");
+		  user.focus();
+		  return false;
+	  } else if(email.value.length == 0 || !mailOk){
+		  alert("Por favor, ingrese su Email");
+		  email.focus();
+		  return false;
+	} return 
+	  alert("Formulario enviado");
+	  formulario.reset();
+	
+}
+
+
 
 
