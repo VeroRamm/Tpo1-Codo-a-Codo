2715c3c4fcce450873e4f1167dc4eea198e07055
Veronica Trias <vero.ramm@gmail.com>
2022-05-05 16:03:10 -0300

    se quito una hooja html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-
 const nombre = document.querySelector('#nombre');
 const apellido = document.querySelector('#apellido');
 const email = document.querySelector('#email');
 
 validaCampos = () => {
-	const nombreValido = nombre.value.trim() !== '';
-	const apellidoValido = apellido.value.trim() !== '';
-	const emailValido = email.value.trim() !== '';
+	const nombreValido = nombre.value.trim() !== '' && nombre.value.trim().length > 2;
+	const apellidoValido = apellido.value.trim() !== '' && apellido.value.trim().length > 2;
+	const emailValido = email.value.trim() !== '' && email.value.includes('@') && email.value.includes('.');
 
 	if (nombreValido && apellidoValido && emailValido) {
 		alert('Formulario enviado');
